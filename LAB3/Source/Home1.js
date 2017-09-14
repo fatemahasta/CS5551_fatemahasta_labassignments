137ddb279efb2575c1a1f20bbfc67987e7c2d30d
fatemahasta <fatemahasta@hotmail.com>
2017-09-13 21:05:22 -0500

    updated

--- LAB3/Source/Home1.js
+++ LAB3/Source/Home1.js
@@ -4,7 +4,7 @@ var app = angular.module("myVideoSearchApp", []);
 app.controller("videoPageController", function($scope, $http) {
     $scope.searchSting = '';
     $scope.videos = [];
-    $scope.translatedSearchString = '';
+
 
     $scope.getVideos = function () {
         var response = $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + $scope.searchSting + "&type=video&key=AIzaSyByiSJZq3F-tYBKz1jWVZlNMYlq7a-Yybk");
@@ -19,26 +19,18 @@ app.controller("videoPageController", function($scope, $http) {
                 };
             }
         });
-        getTranslation($scope.searchSting);
+        getTranslation();
 
 
     };
-    $scope.translatedSearchString;
+
     var getTranslation = function() {
         var response = $http.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170913T053303Z.4945e7cc7baee1a3.3a5624aea544f1494f6b42ec1d1ee681e74c235d&text="+$scope.searchSting+"&lang=en-ar");
         response.then(function (response) {
+            console.log(response);
             $scope.translatedSearchString =response.data.text[0];
+            console.log(translatedSearchString);
 
-            return $scope.translatedSearchString;
-            $scope.translatedSearchString =document.getElementById('mytext').value;
-           // return translatedSearchString;
-            //console.log(translatedSearchString);
-            //return translatedSearchString;
-            //print(translatedSearchString);
-            // console.log(translatedSearchString);
-            //return(translatedSearchString);
-            // api call here
-            // return translateString;
         });
 
     }
