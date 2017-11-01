812cdd2d616ae4efa1539c33094469f62a193f50
Your Name <you@example.com>
2017-11-01 15:09:31 -0500

    Lab10 submission

---
+++ LAB10/Source/MogoDBConnection/MogoDBConnection/MogoDBConnection/Node/mongo.js
@@ -0,0 +1,155 @@
+/**
+ * Fatema Hasta
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var url = 'mongodb://root:root123@ds125255.mlab.com:25255/asedemo';
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// receive request to insert data
+app.post('/video-details', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        insertDataToDB(db, req.body,
+            function() {
+                res.write("Successfully inserted");
+                res.end();
+            },
+            function(err) {
+                res.write("Insert failed with error " + err);
+                res.end();
+            }
+        );
+    });
+})
+// helper function for data insert
+var insertDataToDB = function (db, data, successCallback, errorCallback) {
+    db.collection('video-details').insertOne(data, function (err, result) {
+        if (err) {
+            errorCallback(err);
+        } else {
+            console.log("Inserted a document into the asedemo collection.");
+            successCallback();
+        }
+    });
+};
+
+// receive request to update data
+app.put('/video-details/:id', function (req, res) {
+    var videoId = req.params.id;
+    var updateObject = {
+        "videoLaunchUrl": req.body['videoLaunchUrl'],
+        "title": req.body['title'],
+        "channelTitle": req.body['channelTitle']
+    };
+    console.log('Sending request with updated params: ' + updateObject.channelTitle);
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        updateDataInDB(db, videoId, updateObject,
+            function (updatedDetails) {
+                res.write("Update success:" + updatedDetails);
+                res.end();
+            },
+            function (errorDetails) {
+                res.write("Failed to update: " + errorDetails);
+                res.end();
+            }
+        );
+    });
+})
+// helper function to update data
+var updateDataInDB = function (db, videoId, updateParams, successCallback, errorCallback) {
+    var paramsToUpdate = { "videoLaunchUrl": updateParams.videoLaunchUrl, "title": updateParams.title, "channelTitle": updateParams.channelTitle };
+    var updatedDetails = db.collection('video-details').findAndModify(
+        {"videoId": videoId},
+        null,
+        {$set: paramsToUpdate},
+        { new: true }
+    );
+    updatedDetails.then(function(data) {
+        successCallback(data);
+    }).catch(function(err) {
+        errorCallback(err);
+    })
+};
+
+// receive request to delete data
+app.delete('/video-details/:id', function (req, res) {
+    var videoId = req.params.id;
+    console.log('Sending mongo request to delete video: ' + videoId);
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        // delete the record here
+        deleteDataFromDB(db, videoId,
+            function (result) {
+                res.write("Successfully deleted " + result + " record(s)");
+                res.end();
+            },
+            function (result) {
+                res.write("Failed to deleted record(s). Error: " + result);
+                res.end();
+            }
+        );
+    });
+})
+// helper function to delete data
+var deleteDataFromDB = function (db, videoId, successCallback, errorCallback) {
+    db.collection('video-details').deleteOne({"videoId": videoId})
+        .then(function(data) {
+            successCallback(data);
+        }).catch(function(data) {
+            errorCallback(data);
+        })
+};
+
+// receive request to get all data
+app.get('/video-details', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        // retrieve the record here
+        findDataFromDB(db, req.body,
+            function (result) {
+                res.send(result);
+            },
+            function (result) {
+                res.send("Failed to fetch data");
+            }
+        );
+    });
+})
+// helper function to find all data
+var findDataFromDB = function (db, requestBody, successCallback, errorCallback) {
+    var cursor = db.collection('video-details').find();
+    var result = cursor.toArray();
+    result.then(function(data) {
+        successCallback(data);
+    });
+};
+
+var server = app.listen(8081,function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ LAB10/Source/MogoDBConnection/MogoDBConnection/MogoDBConnection/js/index.js
@@ -0,0 +1,124 @@
+/**
+ * Created by user on 23/10/2016.
+ */
+var myapp = angular.module('demoMongo',[]);
+myapp.run(function ($http) {
+    // Sends this header with any AJAX request
+    $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    // Send this header only in post requests. Specifies you are sending a JSON object
+    $http.defaults.headers.post['dataType'] = 'json'
+});
+myapp.controller('MongoRestController',function($scope,$http) {
+    $scope.searchSting = '';
+    $scope.videos = [];
+    $scope.savedVideos = [];
+
+    $scope.getVideos = function () {
+        var response = $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + $scope.searchSting + "&type=video&key=AIzaSyByiSJZq3F-tYBKz1jWVZlNMYlq7a-Yybk");
+        response.then(function (response) {
+            for (var i = 0; i < 10; i++) {
+                var videoID = response.data.items[i].id.videoId;
+                $scope.videos[i] = {
+                    "videoId": response.data.items[i].id.videoId,
+                    "snippet": response.data.items[i].snippet,
+                    "videoEmbedUrl": 'http://www.youtube.com/embed/' + videoID,
+                    "videoLaunchUrl": 'https://www.youtube.com/watch?v=' + videoID,
+                    "title": response.data.items[i].snippet.title,
+                    "channelTitle": response.data.items[i].snippet.channelTitle,
+                    "description": response.data.items[i].snippet.description
+                };
+            }
+        });
+    };
+
+    $scope.insertData = function(index) {
+        console.log($scope.videos[index].videoId);
+        console.log($scope.videos[index].title);
+        var dataParams = $scope.videos[index];
+        var config = {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        var req = $http.post('http://127.0.0.1:8081/video-details', dataParams);
+        req.success(function(data, status, headers, config) {
+            $scope.message = data;
+            console.log("inserted" + data);
+            $scope.getAllData();
+        });
+        req.error(function(data, status, headers, config) {
+            alert( "failure message: " + JSON.stringify({data: data}));
+        });
+    };
+
+    $scope.currentEditContext = null;
+
+    // enables edit mode
+    $scope.editData = function(index) {
+        $scope.currentEditContext = $scope.savedVideos[index];
+    }
+
+    // cancel edit mode
+    $scope.cancelEdit = function (index) {
+        $scope.currentEditContext = null;
+        $scope.getAllData();
+    }
+
+    $scope.updateData = function () {
+        var dataParams = $scope.currentEditContext;
+        var videoId = $scope.currentEditContext['videoId'];
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        var req = $http.put('http://127.0.0.1:8081/video-details/' + videoId, dataParams);
+        req.success(function (data, status, headers, config) {
+            $scope.message = data;
+            $scope.getAllData();
+            $scope.cancelEdit();
+        });
+        req.error(function (data, status, headers, config) {
+            alert("failure message: " + JSON.stringify({ data: data }));
+        });
+    };
+
+    $scope.deleteData = function (index) {
+        var thisVideo = $scope.savedVideos[index];
+        var videoId = thisVideo['videoId'];
+        console.log('Sending request to delete video: ' + videoId);
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        var req = $http.delete('http://127.0.0.1:8081/video-details/' + videoId);
+        req.success(function (data, status, headers, config) {
+            $scope.message = data;
+            console.log(data);
+            $scope.getAllData();
+        });
+        req.error(function (data, status, headers, config) {
+            alert("failure message: " + JSON.stringify({ data: data }));
+        });
+    };
+
+    $scope.getAllData = function () {
+        console.log("Fetching saved video details");
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        var req = $http.get('http://127.0.0.1:8081/video-details');
+        req.success(function (data, status, headers, config) {
+            $scope.savedVideos = data;
+        });
+        req.error(function (data, status, headers, config) {
+            alert("Failed to fetch all video data: " + JSON.stringify({ data: data }));
+        });
+    };
+
+    $scope.getAllData();
+});
